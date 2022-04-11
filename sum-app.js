84ecd5fc322253e71d0a0fc1088bd03c8aa2dda0
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-04-11 10:37:41 +0200

    feat: add solution to Kata Sum App

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,21 @@
 class SumApp {
-  // Add you solution here
+  constructor(numbers) {
+    this.numbers = [];
+  }
+
+  addNumber(n) {
+    return this.numbers.push(n);
+  }
+
+  getSum() {
+    let sum = 0;
+    for (let i = 0; i < this.numbers.length; i++) {
+      sum += this.numbers[i];
+    }
+    return sum;
+  }
+
+  reset() {
+    this.numbers = [];
+  }
 }

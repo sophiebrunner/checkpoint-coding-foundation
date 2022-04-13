9d0f571c975b63f8a7e0b7410a730d2ad3c8ec36
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-04-13 15:01:23 +0200

    refactor: insert keys-element in Object.keys and array.includes() to make the Kata finally work

--- filter-api-data.js
+++ filter-api-data.js
@@ -1,38 +1,8 @@
-//Idea 1
 //Filter apiData-array for mandatoryKeys using array.filter(); returns new array with only the filtered entries
+//Filter-parameter checks for every mandatoryKey (spread operator (...) is similar to array.forEach() => iterates over every entry of the array), whether the key is included in the apiData-keys; if yes, the key is returned in the new array filteredData
 function filterApiData(apiData, mandatoryKeys) {
   const filteredData = apiData.filter((keys) => {
-    return Object.keys(apiData) === mandatoryKeys;
+    return Object.keys(keys).includes(...mandatoryKeys);
   });
   return filteredData;
 }
-//Does not work - why?
-
-/*
-//Idea 2
-//Turn objects in apiData into arrays via Object.entries()
-//Compare the apiData-array to the mandatoryKeys array; which mandatoryKeys does it have?
-//If mandatoryKey is in apiData-array, push that into new filteredData-array
-//Convert key:value-pairs to objects again
-
-function filterApiData(apiData, mandatoryKeys) {
-  function apiArray(apiData) {
-    let apiDataArray = [];
-    for (let i = 0; i < apiData; i++) {
-      apiDataArray.push(Object.entries(apiData[i]));
-    }
-    return apiDataArray;
-  }
-
-  function filterForMandatoryKeys(apiDataArray, mandatoryKeys) {
-    let filteredData = [];
-    for (let i = 0; i < apiDataArray; i++) {
-      if (apiDataArray[i] === mandatoryKeys[i]) {
-        filteredData.push(apiDataArray[i]);
-      }
-    }
-    return filteredData;
-  }
-}
-//Also not working
-*/
